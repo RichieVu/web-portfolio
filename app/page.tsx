db87b8faa8c5aa6d2057b0e3a16c01a154cc23a8
VuChu <richievu6693@gmail.com>
2023-06-04 05:13:30 -0700

    update resume information

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-700 to-teal-700 text-white px-4 py-2 rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1oYanPu7OwhMvjonwrBWAzZiOjHt8ckbW/view?usp=sharing"
+                  href="/Richie_Vu_Resume.pdf"
                   target="_blank"
                 >
                   Resume
